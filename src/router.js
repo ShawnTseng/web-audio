8dab5ddc7d48153d0a1f4f22ac0782197e7ab214
Shawn.Tseng <shawn.tseng@hwaotech.com>
2020-08-06 09:20:19 +0800

    feat: 加入路由, 移除預設且不必要的檔案

--- src/router.js
+++ src/router.js
@@ -2,3 +2,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
+
+const routes = [{ path: '/', component: () => import('./views/Index.vue') }];
+
+const router = new VueRouter({
+  routes, // (缩写) 相当于 routes: routes
+});
+
+export default router;
