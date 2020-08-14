368c7c27b74b72a1e841670c4cd3f27af3203691
Shawn.Tseng <shawn.tseng@hwaotech.com>
2020-08-14 09:38:08 +0800

    重新整理程式碼

--- src/views/Chapter2.vue
+++ src/views/Chapter2.vue
@@ -1,11 +1,55 @@
 <template>
   <div>
-    Chapter2
+    is playing? {{ isPlaying }}
+    <button @click="clickHandler">Play / Pause</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioCtx = new AudioContext();
+    const oscillator = audioCtx.createOscillator(); // Audio Source
+    const gainNode = audioCtx.createGain();
+    const filter = audioCtx.createBiquadFilter();
+    return {
+      isPlaying: false,
+      audioCtx,
+      oscillator,
+      gainNode,
+      filter,
+      waveType: 'sine', // sine, square, sawtooth, triangle
+      frequency: 440, // A4
+      detone: 0, // 解諧 可做出和聲
+      volumn: 1,
+    };
+  },
+  methods: {
+    initAudio() {
+      this.oscillator.type = this.waveType;
+      this.oscillator.frequency.value = this.frequency;
+      this.oscillator.detune.value = this.detone;
+      this.gainNode.gain.value = this.volumn;
+    },
+    initConnection() {
+      this.oscillator.connect(this.gainNode);
+      this.oscillator.start();
+    },
+    clickHandler() {
+      if (this.isPlaying) {
+        this.gainNode.disconnect(this.audioCtx.destination);
+      } else {
+        this.gainNode.connect(this.audioCtx.destination);
+      }
+      this.isPlaying = !this.isPlaying;
+    },
+  },
+  created() {
+    this.initAudio();
+    this.initConnection();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
