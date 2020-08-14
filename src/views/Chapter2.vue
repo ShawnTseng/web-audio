6620345283c38ec7c8616ff25d5a8353e7fb4efa
Shawn.Tseng <shawn.tseng@hwaotech.com>
2020-08-14 09:47:14 +0800

    加入濾波器(filter), 重構

--- src/views/Chapter2.vue
+++ src/views/Chapter2.vue
@@ -23,6 +23,10 @@ export default {
       frequency: 440, // A4
       detone: 0, // 解諧 可做出和聲
       volumn: 1,
+      filterType: 'allpass', // lowpass, highpass, bandpass, lowshelf, highshelf, peaking, notch
+      filterF: 350,
+      filterQ: 1,
+      filterGain: 0,
     };
   },
   methods: {
@@ -31,23 +35,35 @@ export default {
       this.oscillator.frequency.value = this.frequency;
       this.oscillator.detune.value = this.detone;
       this.gainNode.gain.value = this.volumn;
+      this.filter.type = this.filterType;
+      this.filter.frequencyValue = this.filterF;
+      this.filter.Q.value = this.filterQ;
+      this.filter.gain.value = this.filterGain;
     },
     initConnection() {
       this.oscillator.connect(this.gainNode);
+      this.gainNode.connect(this.filter);
       this.oscillator.start();
     },
+    play() {
+      this.filter.connect(this.audioCtx.destination);
+    },
+    stop() {
+      this.filter.disconnect(this.audioCtx.destination);
+    },
     clickHandler() {
-      if (this.isPlaying) {
-        this.gainNode.disconnect(this.audioCtx.destination);
-      } else {
-        this.gainNode.connect(this.audioCtx.destination);
-      }
+      this.isPlaying ? this.stop() : this.play();
       this.isPlaying = !this.isPlaying;
     },
   },
   created() {
     this.initAudio();
     this.initConnection();
+  },
+  beforeDestroy() {
+    if (this.isPlaying) {
+      this.filter.disconnect(this.audioCtx.destination);
+    }
   },
 };
 </script>
